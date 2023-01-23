6227617024530fa314eaf22e115ce560a4bd494d
Gerson Martinez <gerson_mtzvilla@hotmail.com>
2023-01-22 21:23:04 -0600

    Cambios en configuracion de cors

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ const corsOptions = {
     } 
 }
 
-app.use(cors(corsOptions))
+app.use(cors({
+    origin: '*'
+}));
 
 app.use( '/api/veterinarios', veterinarioRoutes )
 app.use( '/api/pacientes', pacienteRoutes )
